ae659dfdd3e16aaeee62cbc3341edf1c840f776a
Bijai <bijai95@gmail.com>
2018-01-20 20:03:14 +0530

    play music working

--- eventPage.js
+++ eventPage.js
@@ -3,5 +3,14 @@ chrome.commands.onCommand.addListener(function(command) {
     if (command == "toggle-feature-play")
     {
         console.log("Play or Pause ? ");
+        chrome.tabs.query({"url":"https://play.google.com/*"}, function (result)
+        {
+            console.log(result);
+            if (result.length == 1)
+            {
+                chrome.tabs.executeScript(result[0].id, {"code":'document.getElementById("player-bar-play-pause").click()'}, function (){})
+
+            }
+        });
     }
   });
