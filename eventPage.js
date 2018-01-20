f8951ac308094e2f39b7afd559afb179e4945ea5
Bijai <bijai95@gmail.com>
2018-01-20 19:45:18 +0530

    Shortcut

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,7 @@
 chrome.commands.onCommand.addListener(function(command) {
     console.log('Command:', command);
+    if (command == "toggle-feature-play")
+    {
+        console.log("Play or Pause ? ");
+    }
   });
